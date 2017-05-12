b4521f4c5e82cc31b55ca373110dd56ca1ac43e7
Michael <michael.ries@gmail.com>
2017-05-12 11:35:25 -0500

    resolve conflict on create_orders migration

--- db/migrate/20140805200233_create_orders.rb
+++ db/migrate/20140805200233_create_orders.rb
@@ -1,11 +1,7 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 86014427dfd82b7f8d3138df731c5097b10a92d5
       t.timestamps null: false
     end
   end
