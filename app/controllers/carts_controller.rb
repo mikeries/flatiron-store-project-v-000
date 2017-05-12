00f6367a5a1d04b12ddbd61c22c6ab64a9070611
bortch89 <bortch@gmail.com>
2017-05-12 21:23:51 +0200

    Working on cartscontroller, redirecting to show page on checkout works

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,15 @@
 class CartsController < ApplicationController
   def show
-    
+    @checked_out = false
+  end
+
+  def checkout
+    @cart = Cart.find(params[:id])
+    @checked_out = true
+    @cart.line_items.each do |line_item|
+      line_item.item.inventory = line_item.item.inventory - line_item.quantity
+    end
+    @cart.save
+    redirect_to cart_path(@cart)
   end
 end
