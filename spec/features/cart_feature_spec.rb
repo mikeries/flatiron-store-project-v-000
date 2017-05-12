8c5e3bd986695d177edd58899efcf375e2359aa6
Michael <michael.ries@gmail.com>
2017-05-12 12:03:15 -0500

    add dummy user to test framework

--- spec/features/cart_feature_spec.rb
+++ spec/features/cart_feature_spec.rb
@@ -1,4 +1,7 @@
 describe 'Feature Test: Cart', :type => :feature do
+  before(:each) do
+    FactoryGirl.create(:user)
+  end
 
   describe "Checking out" do
 
@@ -52,7 +55,7 @@ describe 'Feature Test: Cart', :type => :feature do
        click_button("Checkout")
 
        @user.reload
-       expect(@user.current_cart).to be_nil 
+       expect(@user.current_cart).to be_nil
      end
     end
   end
@@ -140,7 +143,7 @@ describe 'Feature Test: Cart', :type => :feature do
 
       it "Updates quantity when selecting the same item twice" do
         first_item = Item.first
-        2.times do 
+        2.times do
           visit store_path
           within("form[action='#{line_items_path(item_id: first_item)}']") do
             click_button("Add to Cart")
